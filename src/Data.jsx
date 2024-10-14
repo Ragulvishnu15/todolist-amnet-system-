ef9349593cc67a0f8191fadb95a45b67f5ad17a2
Ragulvishnu15 <ragulvishnu.r@gmail.com>
2024-10-14 15:43:40 +0530

    Add files via upload

---
+++ src/Data.jsx
@@ -0,0 +1,28 @@
+export const userList = [
+    {
+        name : "RAGULVISHNU",
+        email: "ragulvishnu@gmail.com",
+        id: 1
+    },
+    {
+        name : "ragul",
+        email: "ragul15@gmail.com",
+        id: 2
+    },
+    {
+        name : "vishnu",
+        email: "vishnu11@gmail.com",
+        id: 3
+    },
+    {
+        name : "gopal",
+        email: "gopal11@gmail.com",
+        id: 3
+    },
+    {
+        name : "raja",
+        email: "raja11@gmail.com",
+        id: 3
+    },
+    
+]
